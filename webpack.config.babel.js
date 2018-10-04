bf857c7b26a4b33d2ad00ee34f866d14244b773c
Rafael Specht <rafaelspdasilva@gmail.com>
2018-10-04 09:07:55 -0300

    Converting to spaces because we are not savages

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,30 +3,30 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	entry: {
-		main: [
-			'@babel/polyfill',
-			'./src/index.js',
-		]
-	},
-	mode: 'development',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader',
-			}
-		}]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
+  entry: {
+    main: [
+      '@babel/polyfill',
+      './src/index.js',
+    ]
+  },
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
       template: 'src/index.html'
-		})
-	]
+    })
+  ]
 }
