bb9d6032af21ee4f7df2b971e09c587274e0dfe5
Rafael Specht <rafaelspdasilva@gmail.com>
2018-10-07 14:47:03 -0300

    Adding sass to stack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -15,13 +15,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+          }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
       }
-    }]
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
