1a459a5dd0b571f04991dba497519ad5d30454b0
Rafael Specht <rafaelspdasilva@gmail.com>
2018-10-10 08:58:22 -0300

    Adding React and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['standard', 'plugin:react/recommended'],
+  globals: {
+    'fetch': true
+  },
+  rules: {
+    eqeqeq: 'off',
+    'react/display-name': 'off'
+  }
+}
